cc30c677484670397a0ccbb46a1aebc4c46bb86e
Meowmir <disewji@gmail.com>
2024-02-16 10:36:25 +0100

    cards bp

---
+++ src/components/Card.tsx
@@ -0,0 +1,16 @@
+export default function Card(){
+  return (
+    <>
+    <div
+      className="card"
+      style={{
+        backgroundColor: "white",
+        width: 100,
+        height: 150
+    }}>
+      <h4>Card</h4>
+      <p>This is a card.</p>
+    </div>
+    </>
+  )
+}
